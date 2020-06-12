45f9afdfc63206f14e60abb22a9defeea1a74f9d
AriaLink1013 <relix00@gmail.com>
2020-06-12 00:55:40 -0500

    Primer commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CKEditorModule } from 'ckeditor4-angular';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'test';
+}
